d66352400243af73ac71a51b9f490b819f59e355
Katya Chuprina <katyachuprina@MacBook-Air-Katya.local>
2023-01-21 14:51:07 +0300

    docs: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strLength1 = str.length;
+  let strLength2 = '';
+
+  while (strLength2 !== strLength1) {
+    strLength1 = str.length;
+    bracketsConfig.forEach(bracketPair => {
+      bracketPair = bracketPair.join('')
+
+      while (str.indexOf(bracketPair) >= 0) {
+        str = str.replace(bracketPair,'')
+      }     
+    }) 
+    strLength2 = str.length
+  }
+    
+  return str.length === 0
 }
